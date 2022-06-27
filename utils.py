0f86d0bb58ff1f09ba800a09a49b6b378d2bd1ae
Andre Lim <64576306+andrepattu@users.noreply.github.com>
2022-06-27 20:53:07 +0100

    first commit

---
+++ utils.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_learning_curve(x, scores, figure_file, window=100):
+    running_avg = np.zeros(len(scores))
+    for i in range(len(running_avg)):
+        running_avg[i] = np.mean(scores[max(0, i-window):(i+1)])
+    plt.ylabel('Score')       
+    plt.xlabel('Episode')   
+    plt.plot(x, running_avg)
+    plt.title(f'Running average of previous {window} scores')
+    plt.savefig(figure_file)
+
+# def plotLearning(scores, filename, x=None, window=5):   
+#     N = len(scores)
+#     running_avg = np.empty(N)
+#     for t in range(N):
+# 	    running_avg[t] = np.mean(scores[max(0, t-window):(t+1)])
+#     if x is None:
+#         x = [i for i in range(N)]
+#     plt.ylabel('Score')       
+#     plt.xlabel('Game')                     
+#     plt.plot(x, running_avg)
+#     plt.savefig(filename)
