20b0a2b3beaa56bd0280292beda924480869cd9c
Andre Lim <64576306+andrepattu@users.noreply.github.com>
2022-08-21 15:43:11 +0100

    plot combined result script and output plots

--- plot-combined-results.py
+++ plot-combined-results.py
@@ -6,7 +6,8 @@ from collections import deque
 
 
 def main():
-    mypath = "./score_logs"
+    env = 'Pendulum'
+    mypath = f"./score_logs/{env}"
     fileNames = [f for f in listdir(mypath) if isfile(join(mypath, f))] 
     all_scores = []
 
@@ -24,17 +25,19 @@ def main():
 
         all_scores.append(average_latest_scores)
     
-    colors = ["b","g","r","c","m","black","y"]
-    x = np.arange(len(all_scores[0]))
+    colors = ["b","g","r"]
+    min_list_len = min([len(lst) for lst in all_scores])
+    x = np.arange(min_list_len)
     ax = plt.subplot(111)
     for i in range(len(all_scores)):
-        ax.plot(x, all_scores[i], label=fileNames[i][:-4], color=colors[i], linewidth=0.8, markersize=2)
+        shortened_list = all_scores[i][:min_list_len]
+        ax.plot(x, shortened_list, label=fileNames[i][:-4], color=colors[i], linewidth=0.8, markersize=2)
 
     ax.legend()
 
     plt.xlabel("Episode")
     plt.ylabel("Last 100 average scores")
-    plt.title("Comparisons of performance of trained target network for all DQN variations")
-    plt.savefig(f"plot/Results.png")
+    plt.title(f"Comparison of training performance of each algorithm in {env}")
+    plt.savefig(f"plots/combined-results/{env}-combined-results.png")
 
 main()
