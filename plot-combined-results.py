8cb446d95adc85812500dc8fe4609ede30c7f39e
Andre Lim <64576306+andrepattu@users.noreply.github.com>
2022-09-02 12:06:41 +0100

    plot script

--- plot-combined-results.py
+++ plot-combined-results.py
@@ -6,13 +6,12 @@ from collections import deque
 
 
 def main():
-    env = 'Pendulum'
+    env = 'MountainCar'
     mypath = f"./score_logs/{env}"
     fileNames = [f for f in listdir(mypath) if isfile(join(mypath, f))] 
     all_scores = []
 
     for filename in fileNames:
-        print(filename)
         fullpath = mypath + "/" + filename
         file = np.loadtxt(fullpath)
         
@@ -25,19 +24,41 @@ def main():
 
         all_scores.append(average_latest_scores)
     
-    colors = ["b","g","r"]
     min_list_len = min([len(lst) for lst in all_scores])
+
+
+    # # do the same for meta-testing scores
+    # mypath2 = mypath + '/meta-testing'
+    # fileNames2 = [f for f in listdir(mypath2) if isfile(join(mypath2, f))]
+    # all_scores2 = dict()
+    # for idx, filename2 in enumerate(fileNames2):
+    #     fullpath = mypath2 + "/" + filename2
+    #     file = np.loadtxt(fullpath)
+        
+    #     latest_scores = deque(maxlen=100) # store the latest 100 scores
+    #     average_latest_scores = []
+
+    #     for row in file:
+    #         latest_scores.append(row)
+    #         average_latest_scores.append(np.mean(latest_scores))
+
+    #     all_scores2[idx] = average_latest_scores[:min_list_len]
+
+    colors = ["b","g","r","c","m"]
     x = np.arange(min_list_len)
     ax = plt.subplot(111)
     for i in range(len(all_scores)):
         shortened_list = all_scores[i][:min_list_len]
         ax.plot(x, shortened_list, label=fileNames[i][:-4], color=colors[i], linewidth=0.8, markersize=2)
 
+    # ax.fill_between(x, all_scores2[0],all_scores2[1],all_scores2[2],all_scores2[3],all_scores2[4])
+    # ax.plot(x, (all_scores2[0]+all_scores2[1]+all_scores2[2]+all_scores2[3]+all_scores2[4])/5, label=f'meta-ppo-{env}-testing', color='y', linewidth=0.8, markersize=2)
+
     ax.legend()
 
     plt.xlabel("Episode")
     plt.ylabel("Last 100 average scores")
-    plt.title(f"Comparison of training performance of each algorithm in {env}")
+    plt.title(f"Comparison of training/meta-testing performances of each algorithm in {env}")
     plt.savefig(f"plots/combined-results/{env}-combined-results.png")
 
 main()
